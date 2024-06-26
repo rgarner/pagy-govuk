4610014e91baebed573f22a1201c19978c81ef27
Russell Garner <rgarner@zephyros-systems.co.uk>
2024-06-26 12:44:58 +0100

    gem bundle pagy-govuk

---
+++ lib/pagy/govuk/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Pagy
+  module Govuk
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/pagy/govuk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pagy::Govuk do
+  it "has a version number" do
+    expect(Pagy::Govuk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
